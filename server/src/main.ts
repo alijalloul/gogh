e03de5140df34459e11e2889d74cb77123bccef6
Ali Jalloul <ali.z.jalloul@gmail.com>
2024-12-24 15:26:15 +0200

    updated cors in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://gogh-v96t-dl1fn4nlb-alijallouls-projects.vercel.app',
+    origin: 'https://gogh-v96t-dl1fn4nlb-alijallouls-projects.vercel.app', // replace with the actual frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // include OPTIONS method
+    allowedHeaders: 'X-Requested-With, Content-Type, Authorization', // make sure to include any custom headers
+    credentials: true,
   });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 5000);
