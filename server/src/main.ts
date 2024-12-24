3046e639a752ada578d8e03bb44565ff0cf6a119
Ali Jalloul <ali.z.jalloul@gmail.com>
2024-12-24 15:23:24 +0200

    added cors headers in the vercel.json

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://gogh-v96t-dl1fn4nlb-alijallouls-projects.vercel.app',
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 5000);
 }
